49b44eda377b393fbe476165dbe6a621f0d7a916
expressroute <expressroute>
2025-12-17 17:25:35 +0000

    New changes

--- notebooks/load_dim_date.py
+++ notebooks/load_dim_date.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+# DBTITLE 1,he e
 from pyspark.sql import functions as F
 
 # ---- Parameters ----
@@ -33,6 +34,7 @@ dim_date = (
     .withColumn("is_weekend", F.col("day_of_week").isin([1, 7]))
 )
 
+
 # ---- Table existence check + write ----
 if not spark.catalog.tableExists(table_name):
     (
@@ -43,5 +45,38 @@ if not spark.catalog.tableExists(table_name):
         .saveAsTable(table_name)
     )
 else:
-    print(f"Table '{table_name}' already exists. No action taken.")
+    print(f"Table '{table_name}' already exists.")
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+# ---- Parameters ----
+table_name = "hen_db.stg.silver_dim_time"
+
+# ---- Generate time dimension (always) ----
+dim_time = (
+    spark.range(0, 24)
+    .withColumnRenamed("id", "hour")
+    .withColumn("time_key", (F.col("hour") * 100).cast("int"))   # 0, 100, ..., 2300
+    .withColumn("hour_24", F.col("hour"))
+    .withColumn("hour_label", F.format_string('%02d:00', F.col("hour")))
+    .select(
+        "time_key",
+        "hour_24",
+        "hour_label"
+    )
+)
+
+# ---- Table existence check + write ----
+if not spark.catalog.tableExists(table_name):
+    (
+        dim_time
+        .write
+        .mode("overwrite")
+        .format("delta")
+        .saveAsTable(table_name)
+    )
+else:
+    print(f"Table '{table_name}' already exists.")
 
